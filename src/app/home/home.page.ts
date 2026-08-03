b4ac7ba61e65405c79b077d7b73eb6c52f94a694
LafuenteColoradoJose <joselafuente77@gmail.com>
2026-08-03 08:23:16 +0200

    feat: añadir aviso temporal de interrupción de servicio ADIF

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { 
-  IonContent,
+  IonContent, IonAlert,
   IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon, IonMenu, IonMenuToggle
 } from '@ionic/angular/standalone';
 import { BoardComponent } from '../components/board/board.component';
@@ -13,7 +13,7 @@ import { optionsOutline } from 'ionicons/icons';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   imports: [
-    IonContent,
+    IonContent, IonAlert,
     IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon, IonMenu, IonMenuToggle,
     BoardComponent, ConfigPanelComponent
   ],
